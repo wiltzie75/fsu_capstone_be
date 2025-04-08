f773892ad2f6fbf7f2f1b76b1ed7dc7878a471cb
Nathaniel Downey <downeyn21@gmail.com>
2025-04-08 12:18:58 -0400

    init

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require("express")
 const app = express();
+const cors = require("cors");
+const prisma = require("./prisma/seed.js");
+
+app.use(cors({ origin: /localhost/ }));
+app.use(express.json());
+app.use(require("morgan")("dev"));
+
+
+app.get("/api/faculty", async (req, res, next) => {
+    try {
+        const faculty = await prisma.faculty.findMany();
+        res.send(faculty)
+    } catch (error) {
+        next(error)
+    }
+})
+
 
 app.listen(3000)
-
-const cors = require("cors");
-app.use(cors({ origin: /localhost/ }));
