a9bb0d263195ae3ce11726e1bb12584e4fbc39ab
Eric Graham <wiltzie.eg@gmail.com>
2025-04-08 12:19:29 -0400

    added app.get and app.delete to server.js

--- server.js
+++ server.js
@@ -1,7 +1,65 @@
 const express = require("express")
 const app = express();
+const cors = require("cors");
+app.use(cors({ origin: /localhost/ }));
+app.use(express.json());
+app.use(require("morgan")("dev"));
+
+app.get("/api/departments", async (req,res, next) => {
+    try {
+        const departments = await prisma.department.findMany();
+        res.json(departments);
+    } catch (err) {
+        next();
+    }
+});
+
+app.post("/api/departments/:id/")
+
+app.put("/api/departments/:id", async (req, res, next) => {
+    try {
+        const id = +req.params.id;
+
+        const departmentExists = await prisma.departments.findUnique({ where: { id } });
+        if (!departmentExists) {
+            return next({
+                status: 404,
+                message: `Could not find department with ${id}. `,
+            });
+        }
+
+        const { name, description, image, email, faculty } = req.body;
+        if (!name || !description || !image || !email || !faculty) {
+            return next({
+                status: 400,
+                message: "Department must have info."
+            });
+        }
+    } catch (err) {
+        
+    }
+})
+app.delete("/api/departments/:departmentId", async (req, res, next) => {
+    try {
+        const id = +req.params.id;
+        const departmentId = +req.params.departmentId;
+
+        const departmentExists = await prisma.department.findFirst({
+            where: { id, departmentId },
+        });
+
+        if (!departmentExists) {
+            return next({
+                status: 404,
+                message: `${departmentId} department does not exist.`,
+            });
+        }
+        await prisma.department.delete({ where: { id, departmentId } });
+        res.sendStatus(204);
+    } catch (err) {
+        next();
+    }
+});
 
 app.listen(3000)
 
-const cors = require("cors");
-app.use(cors({ origin: /localhost/ }));
