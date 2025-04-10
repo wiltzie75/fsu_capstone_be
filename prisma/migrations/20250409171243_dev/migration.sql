091cddfcf9b011d6dfaf437483d8837bb6dfc19c
Eric Graham <wiltzie.eg@gmail.com>
2025-04-10 11:20:09 -0400

    added hashed info

---
+++ prisma/migrations/20250409171243_dev/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
